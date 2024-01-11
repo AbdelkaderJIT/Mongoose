564f9b8d64629b9950e9cd42ac1b9a9d56674820
AbdelkaderJIT <Abdelkader.jerbi.it@gmail.com>
2024-01-11 21:07:54 +0100

    second commit

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.listen(port,(err) => {
     
 })
 
+// Tables Creation.
 
     Person.create([{
         name:"farid",
@@ -30,23 +31,39 @@ app.listen(port,(err) => {
         favoriteFoods:["lablebi","hargma","khmaj"]
     }
 ])
-//.then((el)=> console.log("Person found",el))
 
-Person.find()
-.then(persons => {
-    console.log('people in da restaurant', persons);
-  })
-  .catch(error => {
-    console.error('Error finding persons:', error);
-  });
 
-Person.find({ favoriteFoods: { $in: ['loubeya']} })
+Person.find()  // Display of all the data on the database
+
+Person.find({ favoriteFoods: { $in: ['loubeya']} })  // Display of the person who has a specified food
+
+
+Person.findById({_id:"659b3fb22fafc5a2f6192000"}) // Display of the person with the specified ID 
+
+Person.findByIdAndUpdate({_id:"659b3fb22fafc5a2f6192000"}, {$push: {favoriteFoods:("Houreya")}}, {new:true}) // Find a person by ID then edit the specified food section
+
+Person.findOneAndUpdate({name:"sameh"}, {age:20}) // Set the person's age 20 which is found by name
+
+Person.findOneAndDelete({_id :"659b3fd3bb7fc26032028ce6"})  // Delete the person with the specified ID
+
+Person.deleteMany({ name : 'monji' }) // Delete all the persons with the name monji
   .then(persons => {
-    console.log('Persons with specified foods:', persons);
+    console.log('Persons Deleted are ', persons);
   })
   .catch(error => {
     console.error('Error finding persons:', error);
   });
+
+  var queryChain = function(done) {   // find and sort and limit the documents to 2 then select them and excute the function done
+    var foodToSearch = "loubeya";
+    Person.find({favoriteFoods:"loubeya"}).sort({name : "desc"}).limit(2).select("-age").exec((err, data) => {
+       if(err)
+         done(err);
+      done(null, data);
+    })
+  };
+
+
 
   
   
